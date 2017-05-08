44c6b9a8800fea9c068d32eddb0450c91bdd6a1a
centraltendency <toshanasankar@gmail.com>
2017-05-08 16:49:18 -0700

    Feat: Implemented Random Forest Regressor and Neural Network Regressor
    Inital model creation. Used r2 and mean squared error metrics to determine the performance of the regressors. Optimized PCA at n_components = 8.

--- first.py
+++ first.py
@@ -26,8 +26,8 @@ train['timestamp'] = pd.to_datetime(train['timestamp'])
 
 dataset = train.merge(macro, on = 'timestamp', how = 'left')
 dataset.describe()
-X = dataset.drop(["id", "timestamp"], axis = 1)
-y = dataset["id"]
+X = dataset.drop(["timestamp", "price_doc"], axis = 1)
+y = dataset["price_doc"]
 
 ## One hot encoding data
 from sklearn.preprocessing import LabelEncoder
@@ -60,21 +60,6 @@ for i in X.columns:
 #        name = i + ".png"    
 #        plt.savefig(name)
 #        plt.close()
-#
-#fig, ax = plt.subplots()
-#ax.plot(train.timestamp, train.price_doc)
-#ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
-#
-#scatter_matrix(train)
-#
-#for i in train.columns:
-#    if type(i) != int:
-#        pass
-#    else:
-#        train[i].astype(float)
-#
-#plt.figure()
-#andrews_curves(train, 'timestamp')
 
 
 # PCA
@@ -84,4 +69,74 @@ from sklearn.decomposition import PCA
 pca = PCA()
 
 pca.fit(imputed_X)
-pca.explained_variance_
+pca.explained_variance_ratio_
+
+## Most of the data is explained by the first two principal components.
+
+pca = PCA(n_components = 8).fit(imputed_X)
+reduced_data = pca.transform(imputed_X)
+reduced_data = pd.DataFrame(reduced_data, columns = ["Dimension 1", "Dimension 2", "Dimension 3", "Dimension 4", "Dimension 5", "Dimension 6", "Dimension 7", "Dimension 8"])
+
+## Split data
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(reduced_data, y)
+
+
+## Random Forest
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+clf_rf = RandomForestRegressor()
+clf_rf.fit(X_train, y_train)
+
+clf_r2 = r2_score(y_test, clf_rf.predict(X_test)) # 0.34062819632035346
+mse = mean_squared_error(y_test, clf_rf.predict(X_test)) # 14539149684678.707
+
+
+## Neural Networks
+from sknn.mlp import Regressor, Layer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+
+pca = PCA(n_components = 8)
+
+pipeline = Pipeline([
+    ('min/max scalar', MinMaxScaler(feature_range = (0.0, 1.0))),
+    ('neural network', Regressor(layers = [Layer("Rectifier", units = 100), Layer("Linear")], learning_rate = 0.02, n_iter = 10))    
+    ])
+    
+pipeline.fit(X_train, y_train)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
